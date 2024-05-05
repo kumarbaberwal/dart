0a0fd4884805592be4394562f80559d7cde9c3b8
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-06 00:44:02 +0530

    data types and variables

---
+++ datatype-and-variable.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = 'Nitesh';
+  bool male = true;
+  int age = 20;
+  List<String> info = ["Kumar", "Baberwal"];
+  Map<String, dynamic> information = {
+    "name" : name,
+    "age" : age,
+    "male" : male,
+    "list" : info,
+  };
+  print(information);
+}
