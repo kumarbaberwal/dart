752c3f8bb935c74228eabb43a5e0828b4663be36
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-06 22:50:41 +0530

    final and const keyword

--- final-and-const-keyword.dart
+++ final-and-const-keyword.dart
@@ -1,6 +1,7 @@
 void main() {
   // final - run time, can reside inside a class
-  final String name = "Bhavya"; //memory will not be allocated unless you access it
+  final String name =
+      "Bhavya"; //memory will not be allocated unless you access it
   // name = "Yashika"; // shows the error final variable can one be set once!
   print(name);
 
@@ -8,4 +9,13 @@ void main() {
   const String name2 = "Yashika"; //memory will be allocated
   // name2 = "Bhavya"; // shows error constant variable can't be assigned a value!
   print(name2);
+}
+
+class first {
+  final String name = "Yashika";
+  static String name2 = "Bhavya";
+}
+
+class second {
+  String namm = first.name2;
 }
