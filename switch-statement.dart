21af4a8e72e7d4cebcd84a217be7790907047b82
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-10 14:21:04 +0530

    switch statements

---
+++ switch-statement.dart
@@ -0,0 +1,26 @@
+void main() {
+  int a = 14;
+  switch (a) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thrusday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    default:
+      print("Sunday");
+      break;
+  }
+}
