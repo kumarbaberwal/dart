eec0d168ba0cebb3b84313dd9db3e33d9f4cb6e9
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-13 22:59:08 +0530

    extends implements keyword

--- extends-implements-keyword.dart
+++ extends-implements-keyword.dart
@@ -1,1 +1,34 @@
-void main() {}
+void main() {
+  sectionB B = sectionB();
+  B.roll();
+}
+
+class school {
+  void name() {}
+  void age() {}
+}
+
+class college {
+  void roll() {}
+}
+
+class sectionA extends school {
+  // cannot use on multiple classes
+  // inherited the school functions
+}
+
+// can be implemented on multiple classes 
+class sectionB implements school, college {
+  // you need to redefine the shcool and college functions here for the sectionB class
+  void name() {
+    print("Student of Section B");
+  }
+
+  void age() {
+    print("Age of Student in Section B");
+  }
+
+  void roll() {
+    print("This is the Roll No. In College");
+  }
+}
